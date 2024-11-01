e7b145ab7f0c861cebc6d179aaccc5c3748b8c28
Naufil Shaikh <snaufil09@gmail.com>
2024-11-01 08:58:09 +0530

    Remove untracked files

--- Play.Catalog/src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
+++ Play.Catalog/src/Play.Catalog.Service/obj/Debug/net8.0/Play.Catalog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ab19b16c06087bb87a5b0527bf5b172613a1319")]
 [assembly: System.Reflection.AssemblyProductAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Play.Catalog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
