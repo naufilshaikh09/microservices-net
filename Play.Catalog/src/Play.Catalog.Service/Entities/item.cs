f751d53cc5f3e77f8414e9c2907ac2e28b4f4ced
Naufil Shaikh <snaufil09@gmail.com>
2024-11-01 10:28:26 +0530

    Added BsonSerializer configurations

--- Play.Catalog/src/Play.Catalog.Service/Entities/item.cs
+++ Play.Catalog/src/Play.Catalog.Service/Entities/item.cs
@@ -1,11 +1,24 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Play.Catalog.Service.Entities
 {
     public class Item
     {
+        // [BsonId]
+        // [BsonRepresentation(BsonType.String)]
         public Guid Id { get; set; }
+
+        // [BsonElement("name")]
         public string? Name { get; set; }
+
+        // [BsonElement("description")]
         public string? Description { get; set; }
+
+        // [BsonElement("price")]
         public decimal Price { get; set; }
+
+        // [BsonElement("createdDate")]
         public DateTimeOffset CreatedDate { get; set; }
     }
 }
